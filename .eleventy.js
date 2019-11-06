681d0b36cd416f2a05696d114e7a1c0a43521b2b
Alasdair Blackwell <aliblackwell@protonmail.com>
2019-11-06 15:15:50 +0000

    Ensure redirects in dist directory

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(config) {
   // pass some assets right through
   config.addPassthroughCopy("./site/flags")
   config.addPassthroughCopy("./site/css")
+  config.addPassthroughCopy("./site/_redirects")
 
   return {
     dir: {
