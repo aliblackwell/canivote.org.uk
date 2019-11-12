9715fde7ccabf515633abe8436479e9759171458
Alasdair Blackwell <aliblackwell@protonmail.com>
2019-11-12 13:04:25 +0000

    Share box animation

--- site/js/selector-interceptor.js
+++ site/js/selector-interceptor.js
@@ -1,28 +1,24 @@
 function handleCountrySelection(event) {
-  console.log('submitting')
-  event.preventDefault();
-  event.stopPropagation();
-  event.stopImmediatePropagation();
-  
-  let country = event.target[0].value;
+  console.log("submitting")
+  event.preventDefault()
+  event.stopPropagation()
+  event.stopImmediatePropagation()
+
+  let country = event.target[0].value
   let finalPath = `/${country}/`
   window.location = finalPath
 }
 
-let countrySelectorForm = document.querySelector('#country-selector')
+let countrySelectorForm = document.querySelector("#country-selector")
 if (countrySelectorForm) {
-  countrySelectorForm.addEventListener('submit', handleCountrySelection);
+  countrySelectorForm.addEventListener("submit", handleCountrySelection)
 }
 
-
-let sharer = document.querySelector('.sharer ul')
-sharer.style.display = 'none'
+let sharer = document.querySelector(".sharer ul")
+sharer.classList.add("hidden")
 
 function sharePage() {
-  let isVisible = sharer.style.display;
-  if (isVisible === 'block') {
-    sharer.style.display = 'none'
-  } else {
-    sharer.style.display = 'block'
-  }
+  let isVisible = sharer.classList.contains("hidden")
+
+  sharer.classList.toggle("hidden")
 }
