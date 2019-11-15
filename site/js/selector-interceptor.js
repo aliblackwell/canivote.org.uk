eb1301509f160aea5dd54d79f965ab7fab28a37f
Alasdair Blackwell <aliblackwell@protonmail.com>
2019-11-15 09:10:38 +0000

    Imlement final accessibility requirements: skip to content, aria-labelled dropdown, label for search

--- site/js/selector-interceptor.js
+++ site/js/selector-interceptor.js
@@ -19,6 +19,18 @@ sharer.classList.add("hidden")
 
 function sharePage() {
   let isVisible = sharer.classList.contains("hidden")
+  let links = document.querySelectorAll(".share-link")
+  if (isVisible) {
+    sharer.setAttribute("aria-hidden", false)
 
+    for (let i = 0; i < links.length; i++) {
+      links[i].setAttribute("tabindex", 0)
+    }
+  } else {
+    sharer.setAttribute("aria-hidden", true)
+    for (let i = 0; i < links.length; i++) {
+      links[i].setAttribute("tabindex", -1)
+    }
+  }
   sharer.classList.toggle("hidden")
 }
