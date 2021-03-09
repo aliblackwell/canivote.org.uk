bf8f2fde667cc3e33cfe8f646b08901062d061a7
Ali Blackwell <aliblackwell@protonmail.com>
2021-03-09 16:22:00 +0000

    Make redirects work on staging

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(config) {
     config.addPassthroughCopy("./site/flags")
     config.addPassthroughCopy("./site/css")
     config.addPassthroughCopy("./site/img")
+    config.addPassthroughCopy("./site/_redirects")
     config.addPassthroughCopy("./site/js")
     config.addPassthroughCopy("./site/humans.txt")
     config.addPassthroughCopy("./site/favicon.png")
